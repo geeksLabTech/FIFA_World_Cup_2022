9d8f2e32fbaadd041cf5702837b4a2c4e15f82f6
JavierOramas <javiale2000@gmail.com>
2022-11-08 06:25:26 -0500

    loading stats for player from json

--- agent.py
+++ agent.py
@@ -3,25 +3,21 @@ import numpy as np
 class Agent:
     def __init__(self, strategy):
         self.strategy = strategy
-        
-
+    
     def evaluate_actions(self, environment, players):
         pass
 
-    
     def play(self, environment, players):
         pass
 
 
 class RandomAgent(Agent):
 
-    
     def evaluate_actions(self, players):
         selected_actions = []
         for player in players:
             selected_actions.append(np.random.choice(player.actions))
         return selected_actions
 
-    
     def play(self, environment, players):
         return self.evaluate_actions(players)
--- player.py
+++ player.py
@@ -1,8 +1,20 @@
 
+import json
+
 class Player:
-    def __init__(self, name, actions, features):
+    def __init__(self, name, team, actions, features):
         self.name = name
         self.actions = actions
         self.features = features
+        self.team = team
+        self.load_data()
 
-
+    def load_data(self):
+        #  open data.json
+        with open('data.json') as json_file:
+            data = json.load(json_file)
+            self.attack = data[self.team][self.name]['attack']
+            self.creativity = data[self.team][self.name]['creativity']
+            self.defense = data[self.team][self.name]['defense']
+            self.tactical = data[self.team][self.name]['tactical']
+            self.technical = data[self.team][self.name]['technical']
