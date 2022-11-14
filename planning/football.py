6b58743215b7940d55c8c9caf6388ca15e829f73
Lia <liazerqueraf@gmail.com>
2022-11-13 21:54:18 -0500

    PlanningPlroblem update team player

--- planning/football.py
+++ planning/football.py
@@ -1,7 +1,7 @@
 from planning import Action , PlanningProblem
 
 def football_model():
-    return PlanningProblem(initial = 'BallPocesion(P) & PlayerInTeam(P,T)',
+    return PlanningProblem(initial = 'BallPocesion(P) & PlayerInTeam(P,T)  & PlayerInTeam(P1 ,T) & PlayerInTeam(P2,T) & PlayerInTeam(P3,T)  & PlayerInTeam(P4,T) & PlayerInTeam(P5,T) & PlayerInTeam(P6,T) & PlayerInTeam(P2,T) & PlayerInTeam(P7,T) & PlayerInTeam(P8,T) & PlayerInTeam(P9,T) & PlayerInTeam(P10,T) ',
                             goals = 'Postion(B, ZG)',
                             actions = [Action('Move(x,y,z)',
                                             precond = 'Position(z,x) ',
@@ -15,4 +15,4 @@ def football_model():
                                             precond = 'BallPocesion(z) & PlayerInTeam(z,x) & PlayerInTeam(y,x)' ,
                                             effect = '~BallPocesion(z) & BallPocesio(y)',
                                             domain = 'Player(y) & Player(z) & Team(z)')],
-                            domain = 'Player(P) & Team(T)')
+                            domain = 'Player(P) & Team(T) & Player(P1) & Player(P2) & Player(P3) & Player(P4) & Player(P5) & Player(P6) & Player(P7) & Player(P8) & Player(P9) & Player(P10)')
