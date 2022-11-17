b845a672182832af05acfa5256ba9a81138f6f19
Lia <liazerqueraf@gmail.com>
2022-11-17 13:19:06 -0500

    update futbolmodel

--- planning/football.py
+++ planning/football.py
@@ -9,7 +9,7 @@ def football_model(team:Team, player:Player):
             names[f'P{iter}'] = p
             iter += 1
             
-    return names,PlanningProblem(initial = 'BallPocesion(P) & PlayerInTeam(P,T) & PlayerInTeam(P1 ,T) & PlayerInTeam(P2,T) & PlayerInTeam(P3,T)  & PlayerInTeam(P4,T) & PlayerInTeam(P5,T) & PlayerInTeam(P6,T) & PlayerInTeam(P2,T) & PlayerInTeam(P7,T) & PlayerInTeam(P8,T) & PlayerInTeam(P9,T) & PlayerInTeam(P10,T) ',
+    return names,PlanningProblem(initial = 'BallPocesion(P) & Player(P) & Player(P1) & Player(P2) & Player(P3)  & Player(P4) & Player(P5) & Player(P6) & Player(P7) & Player(P8) & Player(P9) & Player(P10) ',
                             goals = 'BallInGoal()',
                             actions = [Action('Move(x,y,z)',
                                             precond = 'Position(z,x) ',
@@ -19,8 +19,8 @@ def football_model(team:Team, player:Player):
                                             precond = 'BallPocesion(z)',
                                             effect = 'BallInGoal() & ~BallPocesion(z)',
                                             domain = 'Player(z)'),
-                                        Action('Pass(x,y,z)',
-                                            precond = 'BallPocesion(z) & PlayerInTeam(z,x) & PlayerInTeam(y,x)' ,
-                                            effect = '~BallPocesion(z) & BallPocesion(y)',
-                                            domain = 'Player(y) & Player(z) & Team(z)')],
-                            domain = 'Player(P) & Team(T) & Player(P1) & Player(P2) & Player(P3) & Player(P4) & Player(P5) & Player(P6) & Player(P7) & Player(P8) & Player(P9) & Player(P10)')
+                                        Action('Pass(x,y)',
+                                            precond = 'BallPocesion(x) & Player(x) & Player(y)' ,
+                                            effect = '~BallPocesion(x) & BallPocesion(y)',
+                                            domain = 'Player(y) & Player(z)')],
+                            domain = 'Player(P) & Player(P1) & Player(P2) & Player(P3) & Player(P4) & Player(P5) & Player(P6) & Player(P7) & Player(P8) & Player(P9) & Player(P10)')
